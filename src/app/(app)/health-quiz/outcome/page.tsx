14b59e5e4dd4b77352ef2e9c78120a5364d33bbf
Miguel Bravo <miguelbravobalestrini@gmail.com>
2024-06-07 15:37:29 -0500

    health quiz outcome screen

--- src/app/(app)/health-quiz/outcome/page.tsx
+++ src/app/(app)/health-quiz/outcome/page.tsx
@@ -6,15 +6,15 @@ import { IoWarningOutline } from 'react-icons/io5';
 import H1 from '@/components/h1';
 import Subtitle from '@/components/subtitle';
 import HealthQuizOutcomeBtn from "@/components/health-quiz-outcome-btn";
+import { HealthQuizData } from '@/lib/types';
 
 export default async function Page() {
   noStore();
   const user = await getUser();
-  // const quizData = JSON.parse(user.quizData);
-  // const isAnyQuestionYes = quizData.some(
-  //   (question) => question.answer === true,
-  // );
-  const isAnyQuestionYes = true;
+  const quizData: HealthQuizData | undefined = user?.healthQuiz && JSON.parse(user.healthQuiz);
+  const isAnyQuestionYes = quizData ? quizData.some(
+    (question) => question.answer === true,
+  ) : false;
 
   return (
     <>
@@ -29,15 +29,15 @@ function QuizOutcome({ isAnyQuestionYes }: { isAnyQuestionYes: boolean }) {
     <div className="flex flex-col flex-1 gap-4 justify-center items-center text-center px-10">
       {isAnyQuestionYes ? (
         <>
-          <IoWarningOutline className="h-20 w-20 text-amber-600" />
+          <IoWarningOutline className="h-16 w-16 text-amber-600" />
           <H1>Check Complete</H1>
           <Subtitle>
-            {`As you answered yes to some of the quizData, we don't recommend cold plunging until you have checked with your medical doctor first.`}
+            {`As you answered yes to some of the questions, we don't recommend cold plunging until you have checked with your medical doctor first.`}
           </Subtitle>
         </>
       ) : (
         <>
-          <FaCheckCircle className="h-20 w-20 text-blue-koldup" />
+          <FaCheckCircle className="h-16 w-16 text-blue-koldup" />
           <H1>Check Complete</H1>
           <Subtitle>
             Based on your answers, cold plunging does not appear to be a health

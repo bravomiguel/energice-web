fc9cf3fd77432318faa3068f750c2c25aa6b35b6
Miguel Bravo <miguelbravobalestrini@gmail.com>
2024-06-11 12:24:18 -0500

    no active code ui logic

--- src/app/(app)/plunge/[unitId]/session/page.tsx
+++ src/app/(app)/plunge/[unitId]/session/page.tsx
@@ -1,7 +1,7 @@
 import { unstable_noStore as noStore } from 'next/cache';
-import { MdFlag, MdReport } from "react-icons/md";
-import { VscReport } from "react-icons/vsc";
-
+import { IoWarningOutline } from 'react-icons/io5';
+import { CiCircleInfo } from 'react-icons/ci';
+import { IoMdInformationCircleOutline } from 'react-icons/io';
 
 import {
   checkAuth,
@@ -14,12 +14,14 @@ import { Input } from '@/components/ui/input';
 import H1 from '@/components/h1';
 import { Button } from '@/components/ui/button';
 import BottomNav from '@/components/bottom-nav';
+import Subtitle from '@/components/subtitle';
+import { cn } from '@/lib/utils';
 
 export default async function Page({ params }: { params: { unitId: string } }) {
   noStore();
 
   // auth check
-  const session = await checkAuth();
+  // await checkAuth();
 
   // get unit
   const unit = await getUnitById(params.unitId);
@@ -40,35 +42,66 @@ export default async function Page({ params }: { params: { unitId: string } }) {
 
   const activeCode = response?.data ?? null;
 
+  if (activeCode) {
+    // logic to create and start session, with 5 second countdown
+  }
+
   return (
-    <main className="relative flex-1 flex flex-col">
-      {/* <Input
-        disabled
-        className='disabled:opacity-100 w-fit'
-        // value={activeCode && activeCode.code ? activeCode.code : ''}
-        value={3108}
-      /> */}
+    <main className="relative flex-1 flex flex-col gap-10">
       <H1>Access Code</H1>
-      <div className="flex-1 flex flex-col justify-center items-center gap-10">
-        <div className="flex flex-col justify-center w-4/6 gap-5">
-          <div className="rounded-md border font-medium border-zinc-500 bg-transparent text-5xl text-center py-3">
-            {3108}
-          </div>
-          <div className="flex flex-col gap-2">
-            <div>
-              <Button className='w-full'>Get new code</Button>
-              <Button variant="outline"><MdReport className='h-5 w-5' /></Button>
-            </div>
-            <Button variant="outline">Report issue</Button>
+      <div className="flex-1 flex flex-col gap-10">
+        <div className="space-y-3 w-full">
+          <Subtitle
+            className={cn('text-zinc-600 flex gap-1 w-full items-center', {
+              invisible: activeCode,
+            })}
+          >
+            <IoMdInformationCircleOutline className="w-4 h-4" />
+            {`No code available`}
+          </Subtitle>
+          <div
+            className={cn(
+              'relative rounded-md border-2 font-medium border-indigo-800 bg-transparent text-center p-5 h-24 w-full flex items-center justify-center',
+              { 'border-green-koldup': !activeCode },
+            )}
+          >
+            {activeCode ? (
+              <>
+                <p className="text-5xl">3108</p>
+                <p className="absolute bottom-0 right-0 text-end text-xs px-2 py-1 text-zinc-500">
+                  Expires in 1:34
+                </p>
+              </>
+            ) : (
+              <div className="flex gap-3">
+                <div className="text-left">
+                  <p>Unlock directly via app</p>
+                  <p className="text-xs text-zinc-500">
+                    NB: may take up to 30 secs
+                  </p>
+                </div>
+                <Button variant="koldupGreen">
+                  Unlock
+                </Button>
+              </div>
+            )}
           </div>
         </div>
-        <div className="w-full h-[25vh] rounded-lg overflow-hidden flex justify-center items-center bg-zinc-200">
-          {`GIF "How it works" Explanation`}
+
+        <div className="space-y-3 w-full">
+          <Subtitle className="text-zinc-600">Instructions</Subtitle>
+          <div className="w-full h-[25vh] rounded-lg overflow-hidden flex justify-center items-center bg-zinc-200">
+            {`GIF "How it works" Explanation`}
+          </div>
         </div>
-        <p>Session starts in: ???</p>
       </div>
-      <BottomNav>
-        <Button className="invisible">For spacing only</Button>
+
+      <BottomNav className="space-y-2">
+        <Subtitle className="text-zinc-600">Access issues?</Subtitle>
+        <Button className="w-full">Unlock directly</Button>
+        <Button variant="outline" className="w-full">
+          Report issue
+        </Button>
       </BottomNav>
     </main>
   );

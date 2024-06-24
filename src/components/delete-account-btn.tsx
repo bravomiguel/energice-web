3276c56342317623954beeb7b966f256c6437ee4
Miguel Bravo <miguelbravobalestrini@gmail.com>
2024-06-24 13:25:16 -0500

    logout and delete confirm modals

--- src/components/delete-account-btn.tsx
+++ src/components/delete-account-btn.tsx
@@ -1,28 +1,73 @@
 'use client';
 
+import { useState, useTransition } from 'react';
+
 import { Button } from './ui/button';
 import { deleteAccount } from '@/actions/actions';
-import { useTransition } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+
+// export default function DeleteAccountBtn() {
+//   const [isPending, startTransition] = useTransition();
+
+//   const handleDeleteAccount = async () => {
+//     startTransition(async () => {
+//       const response = await deleteAccount();
+//       if (response?.error) {
+//         console.error({ error: response.error });
+//       }
+//     });
+//   };
+//   return (
+//     <Button
+//       onClick={async () => await handleDeleteAccount()}
+//       disabled={isPending}
+//       isLoading={isPending}
+//       variant="outline"
+//     >
+//       Delete Account
+//     </Button>
+//   );
+// }
 
 export default function DeleteAccountBtn() {
   const [isPending, startTransition] = useTransition();
-
-  const handleDeleteAccount = async () => {
-    startTransition(async () => {
-      const response = await deleteAccount();
-      if (response?.error) {
-        console.error({ error: response.error });
-      }
-    });
-  };
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Button
-      onClick={async () => await handleDeleteAccount()}
-      disabled={isPending}
-      isLoading={isPending}
-      variant="outline"
-    >
-      Delete Account
-    </Button>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button variant="outline" onClick={() => setIsOpen(true)}>Delete account</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className="mb-5 space-y-5">
+          <DialogTitle className='text-lg leading-tight'>Are you sure you want to delete your account?</DialogTitle>
+          <DialogDescription className='text-base leading-tight'>
+            This action cannot be undone. Your account would be permanently deleted.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="flex flex-col gap-2 mb-5">
+          <Button onClick={() => setIsOpen(false)}>Cancel</Button>
+          <Button
+            variant="outline"
+            onClick={async () => {
+              startTransition(async () => {
+                await deleteAccount();
+              });
+            }}
+            disabled={isPending}
+            isLoading={isPending}
+          >
+            Delete account
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
--- src/components/sign-out-btn.tsx
+++ src/components/sign-out-btn.tsx
@@ -1,22 +1,51 @@
 'use client';
 
+import { useState, useTransition } from 'react';
+
 import { signOutAction } from '@/actions/actions';
 import { Button } from './ui/button';
-import { useTransition } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
 
 export default function SignOutBtn() {
   const [isPending, startTransition] = useTransition();
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <Button
-      onClick={async () => {
-        startTransition(async () => {
-          await signOutAction();
-        });
-      }}
-      disabled={isPending}
-      isLoading={isPending}
-    >
-      Log out
-    </Button>
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button onClick={() => setIsOpen(true)}>Log out</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader className='mb-5'>
+          <DialogTitle>Are you sure you want to log out?</DialogTitle>
+          {/* <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </AlertDialogDescription> */}
+        </DialogHeader>
+        <DialogFooter className='flex flex-col gap-2 mb-5'>
+          <Button
+            onClick={async () => {
+              startTransition(async () => {
+                await signOutAction();
+              });
+            }}
+            disabled={isPending}
+            isLoading={isPending}
+          >
+            Log out
+          </Button>
+          <Button variant="outline" onClick={() => setIsOpen(false)}>
+            Cancel
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }

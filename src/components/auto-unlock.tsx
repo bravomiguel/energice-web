229645b45c4a19c22c02be1bfe6a13831692f00f
Miguel Bravo <miguelbravobalestrini@gmail.com>
2024-07-05 10:26:42 -0500

    fixed unlock error state ui

--- src/components/auto-unlock.tsx
+++ src/components/auto-unlock.tsx
@@ -1,22 +1,24 @@
 'use client';
 
-import { startSession, unlockAction } from '@/actions/actions';
-import { Session, Unit } from '@prisma/client';
 import {
   useCallback,
-  useEffect,
   useLayoutEffect,
   useState,
   useTransition,
 } from 'react';
+import Link from 'next/link';
+import { IoMdInformationCircleOutline } from 'react-icons/io';
+import { PiArrowClockwiseBold } from "react-icons/pi";
+import { toast } from 'sonner';
+import { Session, Unit } from '@prisma/client';
+
+import { startSession, unlockAction } from '@/actions/actions';
 import LoadingSpinner from './loading-spinner';
 import { cn } from '@/lib/utils';
 import BottomNav from './bottom-nav';
 import Subtitle from './subtitle';
 import { Button } from './ui/button';
-import { IoMdInformationCircleOutline } from 'react-icons/io';
 import H1 from './h1';
-import { toast } from 'sonner';
 
 export default function AutoUnlock({
   unitId,
@@ -28,27 +30,24 @@ export default function AutoUnlock({
   className?: string;
 }) {
   const [isPending, startTransition] = useTransition();
-  const [isError, setIsError] = useState<string | null>(null);
+  const [isError, setIsError] = useState<boolean>(false);
 
   const handleUnlock = useCallback(async () => {
     startTransition(async () => {
       const unlockResponse = await unlockAction({ unitId });
       if (unlockResponse?.error) {
-        setIsError(unlockResponse.error);
+        setIsError(true);
         console.error({ error: unlockResponse.error });
         toast.error(unlockResponse.error);
         return;
       }
-
       const startSessionResp = await startSession({ sessionId });
       if (startSessionResp?.error) {
-        setIsError(startSessionResp.error);
+        setIsError(true);
         console.error({ error: startSessionResp.error });
         toast.error(startSessionResp.error);
         return;
       }
-
-      setIsError(null);
     });
   }, [unitId, sessionId]);
 
@@ -77,45 +76,53 @@ export default function AutoUnlock({
   }
 
   if (!isPending && isError) {
-    <>
-      <div className={cn(className)}>
-        <H1>Unlock Plunge</H1>
-        <UnlockRetry handleUnlock={handleUnlock} />
-      </div>
-      <BottomNav className="gap-2">
-        <Subtitle className="text-zinc-600 text-start">Access issues?</Subtitle>
-        <Button variant="outline" className="w-full" onClick={() => {}}>
-          Report issue
-        </Button>
-      </BottomNav>
-    </>;
+    return (
+      <>
+        <div className={cn(className)}>
+          <H1>Unlock Plunge</H1>
+          <UnlockRetry handleUnlock={handleUnlock} isPending={isPending} />
+        </div>
+        <BottomNav className="gap-2">
+          <Subtitle className="text-zinc-600 text-start">
+            Access issues?
+          </Subtitle>
+          <Link href="https://koldup.com/#help-and-support" className="w-full">
+            <Button variant="outline" className="w-full">
+              Report issue
+            </Button>
+          </Link>
+        </BottomNav>
+      </>
+    );
   }
 
   return null;
 }
 
-function UnlockRetry({ handleUnlock }: { handleUnlock: () => Promise<void> }) {
+function UnlockRetry({
+  handleUnlock,
+  isPending,
+}: {
+  handleUnlock: () => Promise<void>;
+  isPending: boolean;
+}) {
   return (
     <div className="flex-1 flex flex-col items-center gap-3">
       <Subtitle className="text-zinc-600 flex gap-1 w-full items-center">
         <IoMdInformationCircleOutline className="w-4 h-4" />
-        {`Unlock unsuccessful, please try again`}
+        Unlock unsuccessful
       </Subtitle>
-      <div className="relative rounded-md border-2 font-medium border-green-koldup bg-transparent text-center p-5 h-24 w-full flex items-center justify-between">
-        <div className="text-left">
-          <p>Unlock directly via app</p>
-          <p className="text-xs text-zinc-500">NB: may take up to 30 secs</p>
-        </div>
-        <Button
-          variant="koldupGreen"
-          onClick={async () => {
-            // console.log('hello')
-            await handleUnlock();
-          }}
-        >
-          Unlock
-        </Button>
-      </div>
+      <Button
+        className="w-full h-16 flex gap-3"
+        variant="koldupGreen"
+        onClick={async () => await handleUnlock()}
+        disabled={isPending}
+        isLoading={isPending}
+        loadingBgColor="fill-green-koldup"
+      >
+        <PiArrowClockwiseBold className="w-5 h-5" />
+        <p>Try again</p>
+      </Button>
     </div>
   );
 }
